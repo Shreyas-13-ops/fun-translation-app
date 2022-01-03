c5acdbe2d8b3e969689c0c6a56feb79aea6a6ce9
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-03 15:53:57 +0530

    Add JS: Get reference of Input text, Translate button, Output <div>

---
+++ index.js
@@ -0,0 +1,17 @@
+const inputText = document.getElementById('text');
+const outputDiv = document.getElementById('display-input')
+
+//Translate button
+const translateBtn = document.getElementById('btn-translate');
+
+// const serverURL = fetch('');
+
+// translateBtn.addEventListener('click', clickHandler)
+
+// function clickHandler(){
+//     outputDiv.textContent = getTranslation(text.value);
+// }
+
+// function getTranslation(text){
+//     return `Translated into this ---> ${text}`
+// }
