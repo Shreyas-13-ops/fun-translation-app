5bd4b899d2dde305d8f153a2cdb6a2485f99ab9b
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-03 19:04:56 +0530

    Add translate functionality

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const inputText = document.getElementById('text');
-const outputDiv = document.getElementById('display-input')
-
-//Translate button
+const outputDiv = document.getElementById('display-output')
 const translateBtn = document.getElementById('btn-translate');
 
-translateBtn.addEventListener('click', clickHandler)
+translateBtn.addEventListener('click', clickHandler);
+
+// API limit 5 requests/hour
+let URL = 'https://api.funtranslations.com/translate/yoda.json';
+// For testing - open
+let serverURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+
 
 function clickHandler(){
-    outputDiv.textContent = getTranslation(text.value);
+    //read the value of input text
+    let input = inputText.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(input))
+    .then(response => response.json())
+    .then(json => {
+        //store value of key=translated
+        let translatedText = json.contents.translated;
+        //add the value to DOM element
+        outputDiv.innerText = translatedText;})
+    .catch(err => console.log(`Error occured --> ${err}`))
 }
 
-function getTranslation(text){
-    return `Translated into this ---> ${text}`
+function getTranslationURL(text){
+    return `${URL}?text=${text}`;
 }
